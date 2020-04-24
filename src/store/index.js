66b78754b7231c9b987884999a30f88dad05800e
xiemengbi <xiemengbi@ebupt.com>
2020-04-24 14:53:51 +0800

    登录页

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,14 @@ import Vuex from 'vuex'
 import user from './modules/user'
 import getters from './getters'
 import permission from './modules/permission'
+import common from './modules/common'
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules: {
         user,
         permission,
+        common
     },
     getters
 });
